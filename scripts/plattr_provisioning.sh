867d1a0b4bfa8de382a536aecee27049ff1c6f1e
Will Jackson <wjackson64@gmail.com>
2014-11-17 17:08:52 +0000

    As the comment in plattr_provisioning says, adding tapas.conf to conf.d doesn't work because the override has to be applied after a particular line in httpd.conf - fixing.

--- scripts/plattr_provisioning.sh
+++ scripts/plattr_provisioning.sh
@@ -160,10 +160,9 @@ echo "Setting up tapas"
 # The `Include conf.d/*.conf` line in httpd.conf occurs before the directive, 
 # so any changes made to it that way fail.  
 tapas_conf="Include /home/vagrant/requirements/tapas.conf" 
-
-if [ ! -f /etc/httpd/conf.d/tapas.conf ]; then 
+if ! grep -q $tapas_conf /etc/httpd/conf/httpd.conf; then  
   echo "Configuring httpd.conf"
-  sudo cp /home/vagrant/requirements/tapas.conf /etc/httpd/conf.d/
+  echo $tapas_conf | sudo tee --append  /etc/httpd/conf/httpd.conf >/dev/null
 fi
 
 echo "Symlinking vagrant config files" 
