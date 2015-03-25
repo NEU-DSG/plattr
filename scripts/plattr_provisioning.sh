cbc9d5025a7ba14e6afb553d5c14e076f2af77ae
Will Jackson <wjackson64@gmail.com>
2015-03-25 15:24:11 -0400

    Remove commented out code.

--- scripts/plattr_provisioning.sh
+++ scripts/plattr_provisioning.sh
@@ -33,18 +33,12 @@ if rake jetty:status | grep -q "^Running"; then
 fi
 
 thor drupal_jetty:init
-#thor exist_jetty:init
 
 # rake jetty:start throws an ugly/alarming error if called when the 
 # server is already running, so check first. 
 if rake jetty:status | grep -q "Not running"; then 
   rake jetty:start
 fi
-# So that eXist works better out-of-the-box, set its admin password
-# and change its default database permissions.
-#echo "Giving Jetty some time to start"
-#sleep 20s
-#thor exist_jetty:set_permissions
 
 echo "Starting Redis" 
 sudo service redis start 
